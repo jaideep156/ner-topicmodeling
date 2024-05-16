486d1b7a57f1ce353c02cc4e02c09d65c9ff95cd
Jaideep Prashar <jaideep156@gmail.com>
2024-05-16 15:41:28 +0530

    fixed MediaFileStorageError

--- pages/1_📈EDA.py
+++ pages/1_📈EDA.py
@@ -1,8 +1,5 @@
 import streamlit as st
 import pandas as pd
-from collections import Counter
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
 
 st.markdown("# Exploratory Data Analysis📊")
 st.sidebar.markdown(" ## Exploratory Data Analysis📊")
@@ -18,17 +15,17 @@ st.markdown(f"Below are random 5 observations from [{file_name}](https://github.
 st.write(df.sample(5))
 
 st.markdown("#### Word cloud of the complete data.")
-st.image('notebook/EDA/wordcloud_all.png', use_column_width=True)
+st.image('notebook/EDA_images/wordcloud_all.png', use_column_width=True)
 
 st.markdown("#### Bigram Word cloud of the complete data.")
-st.image('notebook/EDA/wordcloud_bigram_all.png', use_column_width=True)
+st.image('notebook/EDA_images/wordcloud_bigram_all.png', use_column_width=True)
 
 st.markdown("#### Bar chart of the 15 most frequent words in the data.")
-st.image('notebook/EDA/barchart_all.png', use_column_width=True)
+st.image('notebook/EDA_images/barchart_all.png', use_column_width=True)
 
 st.markdown("#### Category wise bar chart of the 15 most frequent words in the data.")
-st.image('notebook/EDA/top15_technology.png', use_column_width=True)
-st.image('notebook/EDA/top15_business.png', use_column_width=True)
-st.image('notebook/EDA/top15_sports.png', use_column_width=True)
-st.image('notebook/EDA/top15_education.png', use_column_width=True)
-st.image('notebook/EDA/top15_entertainment.png', use_column_width=True)
+st.image('notebook/EDA_images/top15_technology.png', use_column_width=True)
+st.image('notebook/EDA_images/top15_business.png', use_column_width=True)
+st.image('notebook/EDA_images/top15_sports.png', use_column_width=True)
+st.image('notebook/EDA_images/top15_education.png', use_column_width=True)
+st.image('notebook/EDA_images/top15_entertainment.png', use_column_width=True)
